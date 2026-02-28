84ffcfdb72cfa03ff852260b35b90c949f333523
LiVNLOUD <akdagknmain@gmail.com>
2026-02-28 20:42:16 +0300

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -1,1 +1,19 @@
+# ==========================================
+# LiVNLOUD - System Cleaner Firlatici
+# ==========================================
+Clear-Host
+Write-Host "LIVN.TR Sunucusuna Baglaniliyor..." -ForegroundColor Cyan
 
+# 1. Calisma Klasörü (Temp)
+$workDir = "$env:TEMP\LiVNLOUD"
+if (!(Test-Path $workDir)) { New-Item -ItemType Directory -Path $workDir | Out-Null }
+
+# 2. Senin Ana Scriptinin Raw Linki
+$scriptUrl = "https://raw.githubusercontent.com/LiVNLOUD/livn-tools/main/_Files/SystemCleaner.ps1"
+
+# 3. Scripti Indir ve Calistir
+Write-Host "[>] Sistem hazirlaniyor..." -ForegroundColor Yellow
+Invoke-WebRequest -Uri $scriptUrl -OutFile "$workDir\SystemCleaner.ps1" -ErrorAction SilentlyContinue
+
+# 4. Scripti Atesle
+& "$workDir\SystemCleaner.ps1"
